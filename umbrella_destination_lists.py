1eb28ac59ebad42499d59d688f2200f22cb9db21
Ryan Wehe <ryan@ryanwehe.com>
2022-05-19 01:23:19 -0700

    function to GET the Destination Lists

---
+++ umbrella_destination_lists.py
@@ -0,0 +1,23 @@
+import requests
+import json
+#from umbrella_api_tools import organizationId, management_key, management_secret
+
+base_uri = 'https://management.api.umbrella.com/v1'
+
+
+def getDestinationLists(organizationId, management_key, management_secret):
+    url = f'{base_uri}/organizations/{organizationId}/destinationlists'
+    r = requests.get(url, headers={'Accept': 'application/json'}, auth=(management_key,management_secret))
+    data = json.loads(r.content)
+    print('''
+***********************************************************************************************
+*                                 Destination Lists Found                                     *
+*_____________________________________________________________________________________________*
+***********************************************************************************************
+''')
+    destinationLists = []
+    for i in range(len(data['data'])):
+        destinationLists.append(data['data'][i]['name'])
+    print(*destinationLists, sep="\n")
+    return(destinationLists)
+    
