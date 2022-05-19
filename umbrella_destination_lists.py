5f72b3c708f8d616b2b3d2e2fd88d0002630a382
Ryan Wehe <ryan@ryanwehe.com>
2022-05-19 01:28:31 -0700

    clean line breaks

--- umbrella_destination_lists.py
+++ umbrella_destination_lists.py
@@ -3,7 +3,6 @@ import json
 
 base_uri = 'https://management.api.umbrella.com/v1'
 
-
 def getDestinationLists(organizationId, management_key, management_secret):
     url = f'{base_uri}/organizations/{organizationId}/destinationlists'
     r = requests.get(url, headers={'Accept': 'application/json'}, auth=(management_key,management_secret))
@@ -18,5 +17,4 @@ def getDestinationLists(organizationId, management_key, management_secret):
     for i in range(len(data['data'])):
         destinationLists.append(data['data'][i]['name'])
     print(*destinationLists, sep="\n")
-    return(destinationLists)
-    
+    return(destinationLists)    
